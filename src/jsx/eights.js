19d6f90433580141fb396d0c274936dce77f729c
George Nishimura <thetmkay@gmail.com>
2015-04-16 15:17:30 +0100

    added nav/banner/header and renamed to fav

--- src/jsx/eights.js
+++ src/jsx/eights.js
@@ -1,36 +1,36 @@
-        //var section_template = require('../section_templates/eight.rt.js');
-        var section_template = require('../templates/eight-section.rt.js');
-        var header_template = require('../templates/eight-nav.rt.js');
+        //var section_template = require('../section_templates/fav.rt.js');
+        var section_template = require('../templates/fav-section.rt.js');
+        var header_template = require('../templates/fav-nav.rt.js');
         var React = require('react/addons');
 
         var movies = [
-            'The Dark Knight',
-            'Princess Mononoke',
-            'Ocean\'s 11',
-            'In Bruges',
-            'Love Actually',
-            '500 Days of Summer',
-            'Lost in Translation',
-            'Something Else'
+            { title: 'The Dark Knight'},
+            { title: 'Princess Mononoke'},
+            { title: 'Ocean\'s 11'},
+            { title: 'In Bruges'},
+            { title: 'Love Actually'},
+            { title: '500 Days of Summer'},
+            { title: 'Lost in Translation'},
+            { title: 'Something Else'}
         ];
 
         var sections = [{
             title: 'movies',
-            'eight': movies
+            'picks': movies
         }];
 
         var Section = React.createClass({
             getInitialState: function() {
                 return {
-                    title: 'movies',
-                    eight: movies
+                    title: this.props.title,
+                    picks: this.props.picks
                 };
             },
             render: function() {
                 return section_template.apply(this);
             }
         });
-        var EightNav = React.createClass({
+        var FavNav = React.createClass({
             render: function() {
               return header_template.apply(this);
             }
@@ -38,15 +38,16 @@
         });
         var Lists = React.createClass({
             render: function() {
-                var eight_sections = this.props.sections.map(function(section) {
+                var fav_sections = this.props.sections.map(function(section) {
+                    section.picks[0].selected = true;
                     return ( 
-                      <Section title={section.title} eight={section.eight} />
+                      <Section title={section.title} picks={section.picks} />
                     );
                 });
                 return ( 
                    <div className="sections-list" >
-                    <EightNav /> 
-                    {eight_sections}
+                    <FavNav /> 
+                    {fav_sections}
                    </div>
                 );
             }
